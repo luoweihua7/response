22c48471dbba74da85c2df7b191d452db6cf5f0f
luoweihua7 <luowehua7@gmail.com>
2020-03-27 22:29:53 +0800

    add: AccuWeather response

---
+++ AccuWeather/alerts.js
@@ -0,0 +1,45 @@
+[
+  {
+    AlertID: 162307,
+    Area: [
+      {
+        EndTime: '2020-01-14T10:00:00+08:00',
+        EpochEndTime: 1578967200,
+        EpochStartTime: 1578880620,
+        LanguageCode: 'zh-cn',
+        LastAction: {
+          English: 'Update',
+          Localized: '更新',
+        },
+        Name: '安阳市',
+        StartTime: '2020-01-13T09:57:00+08:00',
+        Summary: '霾橙色预警 生效，持续时间至 星期二，10:00上午 CST。来源：国家预警信息发布中心',
+        Text: '安阳市气象台2020年1月13日10时00分继续发布霾橙色预警信号：预计未来24小时内，安阳市有中到重度霾，请注意防范。',
+      },
+    ],
+    Category: 'NON-PRECIPITATION',
+    Class: null,
+    Color: {
+      Blue: 0,
+      Green: 165,
+      Hex: '#FFA500',
+      Name: 'Orange',
+      Red: 255,
+    },
+    CountryCode: 'CN',
+    Description: {
+      English: 'Orange Warning of Haze',
+      Localized: '霾橙色预警',
+    },
+    Disclaimer: null,
+    HaveReadyStatements: true,
+    Level: '橙色',
+    Link: 'http://www.accuweather.com/zh/cn/anyang/59083/weather-warnings/59083?lang=zh-cn',
+    MobileLink: 'http://m.accuweather.com/zh/cn/anyang/59083/weather-warnings/59083?lang=zh-cn',
+    Priority: 206,
+    Source: '国家预警信息发布中心',
+    SourceId: 62,
+    Type: '霾',
+    TypeID: 'CN225',
+  },
+]
---
+++ AccuWeather/currentconditions.js
@@ -0,0 +1,382 @@
+[
+  {
+    ApparentTemperature: {
+      Imperial: {
+        Unit: 'F',
+        UnitType: 18,
+        Value: 67.0,
+      },
+      Metric: {
+        Unit: 'C',
+        UnitType: 17,
+        Value: 19.4,
+      },
+    },
+    Ceiling: {
+      Imperial: {
+        Unit: 'ft',
+        UnitType: 0,
+        Value: 30000.0,
+      },
+      Metric: {
+        Unit: 'm',
+        UnitType: 5,
+        Value: 9144.0,
+      },
+    },
+    CloudCover: 91,
+    DewPoint: {
+      Imperial: {
+        Unit: 'F',
+        UnitType: 18,
+        Value: 52.0,
+      },
+      Metric: {
+        Unit: 'C',
+        UnitType: 17,
+        Value: 10.9,
+      },
+    },
+    EpochTime: 1578905280,
+    HasPrecipitation: false,
+    IsDayTime: true,
+    Link: 'http://www.accuweather.com/zh/cn/shenzhen/58194/current-weather/58194?lang=zh-cn',
+    LocalObservationDateTime: '2020-01-13T16:48:00+08:00',
+    LocalSource: {
+      Id: 7,
+      Name: '华风',
+      WeatherCode: '01',
+    },
+    MobileLink: 'http://m.accuweather.com/zh/cn/shenzhen/58194/current-weather/58194?lang=zh-cn',
+    ObstructionsToVisibility: '',
+    Past24HourTemperatureDeparture: {
+      Imperial: {
+        Unit: 'F',
+        UnitType: 18,
+        Value: 4.0,
+      },
+      Metric: {
+        Unit: 'C',
+        UnitType: 17,
+        Value: 2.3,
+      },
+    },
+    Precip1hr: {
+      Imperial: {
+        Unit: 'in',
+        UnitType: 1,
+        Value: 0.0,
+      },
+      Metric: {
+        Unit: 'mm',
+        UnitType: 3,
+        Value: 0.0,
+      },
+    },
+    PrecipitationSummary: {
+      Past12Hours: {
+        Imperial: {
+          Unit: 'in',
+          UnitType: 1,
+          Value: 0.0,
+        },
+        Metric: {
+          Unit: 'mm',
+          UnitType: 3,
+          Value: 0.0,
+        },
+      },
+      Past18Hours: {
+        Imperial: {
+          Unit: 'in',
+          UnitType: 1,
+          Value: 0.0,
+        },
+        Metric: {
+          Unit: 'mm',
+          UnitType: 3,
+          Value: 0.0,
+        },
+      },
+      Past24Hours: {
+        Imperial: {
+          Unit: 'in',
+          UnitType: 1,
+          Value: 0.0,
+        },
+        Metric: {
+          Unit: 'mm',
+          UnitType: 3,
+          Value: 0.0,
+        },
+      },
+      Past3Hours: {
+        Imperial: {
+          Unit: 'in',
+          UnitType: 1,
+          Value: 0.0,
+        },
+        Metric: {
+          Unit: 'mm',
+          UnitType: 3,
+          Value: 0.0,
+        },
+      },
+      Past6Hours: {
+        Imperial: {
+          Unit: 'in',
+          UnitType: 1,
+          Value: 0.0,
+        },
+        Metric: {
+          Unit: 'mm',
+          UnitType: 3,
+          Value: 0.0,
+        },
+      },
+      Past9Hours: {
+        Imperial: {
+          Unit: 'in',
+          UnitType: 1,
+          Value: 0.0,
+        },
+        Metric: {
+          Unit: 'mm',
+          UnitType: 3,
+          Value: 0.0,
+        },
+      },
+      PastHour: {
+        Imperial: {
+          Unit: 'in',
+          UnitType: 1,
+          Value: 0.0,
+        },
+        Metric: {
+          Unit: 'mm',
+          UnitType: 3,
+          Value: 0.0,
+        },
+      },
+      Precipitation: {
+        Imperial: {
+          Unit: 'in',
+          UnitType: 1,
+          Value: 0.0,
+        },
+        Metric: {
+          Unit: 'mm',
+          UnitType: 3,
+          Value: 0.0,
+        },
+      },
+    },
+    PrecipitationType: null,
+    Pressure: {
+      Imperial: {
+        Unit: 'inHg',
+        UnitType: 12,
+        Value: 30.0,
+      },
+      Metric: {
+        Unit: 'mb',
+        UnitType: 14,
+        Value: 1016.0,
+      },
+    },
+    PressureTendency: {
+      Code: 'S',
+      LocalizedText: '稳定',
+    },
+    RealFeelTemperature: {
+      Imperial: {
+        Unit: 'F',
+        UnitType: 18,
+        Value: 66.0,
+      },
+      Metric: {
+        Unit: 'C',
+        UnitType: 17,
+        Value: 19.0,
+      },
+    },
+    RealFeelTemperatureShade: {
+      Imperial: {
+        Unit: 'F',
+        UnitType: 18,
+        Value: 66.0,
+      },
+      Metric: {
+        Unit: 'C',
+        UnitType: 17,
+        Value: 19.0,
+      },
+    },
+    RelativeHumidity: 55,
+    Temperature: {
+      Imperial: {
+        Unit: 'F',
+        UnitType: 18,
+        Value: 68.0,
+      },
+      Metric: {
+        Unit: 'C',
+        UnitType: 17,
+        Value: 20.2,
+      },
+    },
+    TemperatureSummary: {
+      Past12HourRange: {
+        Maximum: {
+          Imperial: {
+            Unit: 'F',
+            UnitType: 18,
+            Value: 70.0,
+          },
+          Metric: {
+            Unit: 'C',
+            UnitType: 17,
+            Value: 21.0,
+          },
+        },
+        Minimum: {
+          Imperial: {
+            Unit: 'F',
+            UnitType: 18,
+            Value: 57.0,
+          },
+          Metric: {
+            Unit: 'C',
+            UnitType: 17,
+            Value: 14.0,
+          },
+        },
+      },
+      Past24HourRange: {
+        Maximum: {
+          Imperial: {
+            Unit: 'F',
+            UnitType: 18,
+            Value: 70.0,
+          },
+          Metric: {
+            Unit: 'C',
+            UnitType: 17,
+            Value: 21.0,
+          },
+        },
+        Minimum: {
+          Imperial: {
+            Unit: 'F',
+            UnitType: 18,
+            Value: 57.0,
+          },
+          Metric: {
+            Unit: 'C',
+            UnitType: 17,
+            Value: 14.0,
+          },
+        },
+      },
+      Past6HourRange: {
+        Maximum: {
+          Imperial: {
+            Unit: 'F',
+            UnitType: 18,
+            Value: 70.0,
+          },
+          Metric: {
+            Unit: 'C',
+            UnitType: 17,
+            Value: 21.0,
+          },
+        },
+        Minimum: {
+          Imperial: {
+            Unit: 'F',
+            UnitType: 18,
+            Value: 64.0,
+          },
+          Metric: {
+            Unit: 'C',
+            UnitType: 17,
+            Value: 18.0,
+          },
+        },
+      },
+    },
+    UVIndex: 1,
+    UVIndexText: '低',
+    Visibility: {
+      Imperial: {
+        Unit: 'mi',
+        UnitType: 2,
+        Value: 10.0,
+      },
+      Metric: {
+        Unit: 'km',
+        UnitType: 6,
+        Value: 16.1,
+      },
+    },
+    WeatherIcon: 7,
+    WeatherText: '多云',
+    WetBulbTemperature: {
+      Imperial: {
+        Unit: 'F',
+        UnitType: 18,
+        Value: 58.0,
+      },
+      Metric: {
+        Unit: 'C',
+        UnitType: 17,
+        Value: 14.7,
+      },
+    },
+    Wind: {
+      Direction: {
+        Degrees: 90,
+        English: 'E',
+        Localized: '东',
+      },
+      Speed: {
+        Imperial: {
+          Unit: 'mi/h',
+          UnitType: 9,
+          Value: 10.3,
+        },
+        Metric: {
+          Unit: 'km/h',
+          UnitType: 7,
+          Value: 16.7,
+        },
+      },
+    },
+    WindChillTemperature: {
+      Imperial: {
+        Unit: 'F',
+        UnitType: 18,
+        Value: 68.0,
+      },
+      Metric: {
+        Unit: 'C',
+        UnitType: 17,
+        Value: 20.0,
+      },
+    },
+    WindGust: {
+      Speed: {
+        Imperial: {
+          Unit: 'mi/h',
+          UnitType: 9,
+          Value: 14.2,
+        },
+        Metric: {
+          Unit: 'km/h',
+          UnitType: 7,
+          Value: 22.8,
+        },
+      },
+    },
+  },
+]
